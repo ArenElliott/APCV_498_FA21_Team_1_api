d061afda285e37baaf1947a367fbb44977091cd9
Aren Christopher Elliott <arenchristopherelliott@pop-os.localdomain>
2021-11-21 18:56:42 +0000

    Completed files.json page and hosting. Should display page at /files.json with a json representation of all the files.

--- python/boxsdktest.py
+++ python/boxsdktest.py
@@ -1,8 +1,54 @@
-from boxsdk import DevelopmentClient
-client = DevelopmentClient()
+from boxsdk import OAuth2, Client
+from flask import Flask
+import datetime
+import re
+import json
+from python import config
 
-foldername = '147514427103'
+oauth = OAuth2(
+    client_id=config.client_id,
+    client_secret=config.client_secret,
+)
+client = Client(oauth)
+app = Flask(__name__)
+
+foldername = '125566015976'
 folder = client.folder(folder_id=foldername).get()
 
+yearSearch = re.compile(r'-\d\d(\d\d)')
+
+maxYear = 0
+shortest = 0
+id = ""
+
 for item in folder.item_collection['entries']:
-    print(f"{item.name}, {item.id}")
+    result = yearSearch.search(item.name)
+    thisYear = int(result.group(1))
+    length = len(result.group(0))
+    if (thisYear > maxYear):
+        maxYear = thisYear
+        id = item.id
+    elif ((thisYear == maxYear) and (len(id) > len(item.id))):
+        id = item.id
+
+semesterPlans = json.loads('{"files": []}')
+
+subfolder = client.search().query(query=".pdf", limit=200, file_extensions=['pdf'], ancestor_folder_ids=[id])
+for item in subfolder:
+    file = json.loads('{}')
+    parent_folders = {'parent_folders': [x.name for x in item.path_collection['entries']].copy()}
+    name = {'name': item.name}
+    url = {'url': client.file(item.id).get_url()}
+    file.update(parent_folders)
+    file.update(name)
+    file.update(url)
+    semesterPlans['files'].append(file)
+
+print(json.dumps(semesterPlans, indent = 4))
+
+@app.route("/files.json")
+def index():
+    return str(json.dumps(semesterPlans))
+
+if __name__ == "__main__":
+    app.run()
